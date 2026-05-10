ed381fa6a3135375e0de83212422af5fb78c9aa4
frienddrive6-afk <frienddrive6@gmail.com>
2026-05-10 22:24:59 +0300

    Итоговый проект lesson12 сделал что бы выполняло всем требованиям

--- lesson12/classwork/1_script_classwork.sql
+++ lesson12/classwork/1_script_classwork.sql
@@ -43,7 +43,7 @@ CREATE TABLE roles(
 
 CREATE TABLE permissions(
 	id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	title nvarchar(64) UNIQUE NOT NULL,
+	title nvarchar(64) UNIQUE NOT NULL
 );
 
 
@@ -94,14 +94,26 @@ CREATE TABLE permission_user(
 
 
 
-CREATE TABLES teachers(
+CREATE TABLE teachers(
 	id int PRIMARY KEY NOT NULL,	
 	first_name nvarchar(32) NOT NULL,
 	last_name nvarchar(32) NOT NULL,
-
+	-- .....
 	
 	CONSTRAINT FK_teacher_user FOREIGN KEY(id) REFERENCES users(id)
 );
+
+
+
+CREATE TABLE students(
+	id int PRIMARY KEY NOT NULL,	
+	first_name nvarchar(32) NOT NULL,
+	last_name nvarchar(32) NOT NULL,
+	-- .....
+	
+	CONSTRAINT FK_student_user FOREIGN KEY(id) REFERENCES users(id)
+);
+
 
 
 
@@ -113,11 +125,13 @@ CREATE TABLE pairs(
 	schedule_item_id int NOT NULL,
 	subject_id NOT NULL,
 	theme nvarchar(256) NOT NULL,
-	
+	teacher_id int,
+	teacher_status tinyint,    -- online / offline
 	
 	
 	CONSTRAINT FK_pairs_schedule_item FOREIGN KEY(schedule_item_id) REFERENCES schedule_items(id),
-	CONSTRAINT FK_pairs_subjects FOREIGN KEY(subject_id) REFERENCES subject_id(id),
+	CONSTRAINT FK_pairs_subjects FOREIGN KEY(subject_id) REFERENCES subjects(id),
+	CONSTRAINT FK_pairs_teacher FOREIGN KEY(teacher_id) REFERENCES teachers(id)
 );
 
 
@@ -139,16 +153,87 @@ CREATE TABLE groups_pair(
 );
 
 
+CREATE TABLE group_student (
+	id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	group_id int NOT NULL,
+	student_id int NOT NULL,
+	
+	status tinyint DEFAULT(0) NOT NULL,
+	
+	
+	CONSTRAINT FK_groups_group_student FOREIGN KEY(group_id) REFERENCES groups(id),
+	CONSTRAINT FK_groups_student FOREIGN KEY(student_id) REFERENCES students(id)
+
+);
 
 
 
 
 
 
+CREATE TABLE pair_student (
+	id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	pair_id int NOT NULL,
+	student_id int NOT NULL,
+	status tinyint DEFAULT(0) NOT NULL,   -- присуцтвуетб опоздплб отсуцтвует
+	is_online bit DEFAULT(0) NOT NULL,
+	comment nvarchar(256) NULL,
+	
+	
+	
+	CONSTRAINT FK_pair_pair_student FOREIGN KEY(pair_id) REFERENCES pairs(id),
+	CONSTRAINT FK_pair_student_student FOREIGN KEY(student_id) REFERENCES students(id)
+
+);
 
 
 
 
+
+CREATE TABLE pair_grades(
+	id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	pair_student_id int NOT NULL,
+	val tinyint NOT NULL,
+	val_type tinyint NOT NULL,    -- Классная / контрольная / тематическая оценка
+
+	CONSTRAINT CK_pair_grades_range CHECK(val >= 1 AND val <= 12),
+	CONSTRAINT FK_pair_grades_pair_student FOREIGN KEY(pair_student_id) REFERENCES pair_student(id),
+);
+
+
+
+
+CREATE TABLE homeworks(
+	id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	pair_id int NOT NULL,
+	title nvarchar(256) NOT NULL,
+	description nvarchar(512) NOT NULL,
+	task_file_url nvarchar(512) NULL,
+	deadline_date datetime NOT NULL,
+	
+	CONSTRAINT FK_homeworks_pairs FOREIGN KEY(pair_id) REFERENCES pairs(id)
+);
+
+
+
+CREATE TABLE homeworks_solutions(
+	id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	homework_id int NOT NULL,
+	student_id int NOT NULL,
+	solution_file_url nvarchar(512) NULL,
+	student_comment nvarchar(512) NULL,
+	upload_date datetime DEFAULT() NOT NULL,
+	
+	grade tinyint NULL,
+	teacher_comment nvarchar(512) NULL,
+	check_date datetime NULL,
+	
+	CONSTRAINT CK_homework_grade CHECK(grade >= 1 AND <= 12),
+	CONSTRAINT FK_solutions_homework FOREIGN KEY(homework_id ) REFERENCES homeworks(id),
+	CONSTRAINT FK_solutions_student FOREIGN KEY(student_id) REFERENCES students(id),
+	CONSTRAINT UQ_student_homework UNIQUE(homework_id, student_id)
+
+);
 
 
 
